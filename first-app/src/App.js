c864c133041fd3eb4314276e241992b6d46514a0
noah213 <typicalfilmcrew@gmail.com>
2023-05-26 17:20:44 -0400

    To Do List App

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -4,22 +4,71 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+
+      <main>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Noahs To Do List</h1>
+        
+        <form id="new-task-form">
+          <input type = "text" id="new-task-input" placeholder="What do you have planned?" />
+          <input type="submit" id="new-task-submit" value="Add task" />
+        </form>
+
       </header>
+
+        <section class="task-list">
+          <h2>Tasks</h2>
+          <div id="tasks">
+            <div class="task">
+              <div class="content">
+                <input type = "text" class="text" value = "My shiny task" readonly />
+                <div class="actions">
+                  <button class="edit">Edit</button>
+                  <button class="delete ">Delete</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
     </div>
+
   );
 }
+
+//main js 
+window.addEventListener('load', () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const list_el = document.querySelector("#tasks")
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const task = input.value
+
+    if (!task){
+      alert("Please fill out task!");
+      return
+    }
+
+    const task_el = document.createElement("div");
+    task_el.classList.add("task");
+
+    const task_content_el = document.createElement("div");
+    task_content_el.classList.add("content");
+    task_content_el.innerText = task;
+
+    task_el.appendChild(task_content_el);
+
+    list_el.appendChild(task_el);
+
+
+  })
+
+})
 
 export default App;
